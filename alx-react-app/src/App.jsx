8e05950916fcf153ce828f6dc24ed6a6d16a2ea3
Martha Morufa <marthamarumo92@mail.com>
2025-02-17 01:36:25 +0200

    added the Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <WelcomeMessage />
       <Header/>
       <MainContent/>
+      <Footer/>
     </div>
   )
 }
