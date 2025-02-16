bbee714308857691cce13573389c9ae1815898fb
Martha Morufa <marthamarumo92@mail.com>
2025-02-17 01:15:55 +0200

    updated the app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
+      <Header/>
     </div>
   )
 }
