f71149fb3efc820a9fb9267dfb90788deb1be72b
Martha Morufa <marthamarumo92@mail.com>
2025-02-17 01:32:03 +0200

    added the maincontent

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
+import MainContent from './components/MainContent'
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
       <Header/>
+      <MainContent/>
     </div>
   )
 }
