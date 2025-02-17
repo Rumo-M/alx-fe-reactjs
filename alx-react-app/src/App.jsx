8bd7a62fcb0fa20ac363042c4fa2e4c202d52379
Martha Morufa <marthamarumo92@mail.com>
2025-02-17 14:49:28 +0200

    created the UserProfile and modified the app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import React from 'react';
+import UserProfile from './UserProfile';
+
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
     </div>
   )
 }
