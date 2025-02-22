f3464cfa6c28f994a019afbb115d47bb333a5a76
Martha Morufa <marthamarumo92@mail.com>
2025-02-22 22:13:48 +0200

    modified the userprofile

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@
 const UserProfile = ({name, age, bio}) => {
   return (
     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2>{name}</h2>
+      <h2 style={{color:"blue"}}>{name}</h2>
       <p>Age: <span >{age}</span></p>
       <p>Bio: {bio}</p>  
     </div>
