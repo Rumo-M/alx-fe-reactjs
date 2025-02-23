50e20cca635f2458e09ea011dd5e079ef70b0eb8
Martha Morufa <marthamarumo92@mail.com>
2025-02-23 19:48:26 +0200

    setup the useContext files

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,10 @@
+// src/UserContext.js
+
+import { createContext } from 'react';
+
+// Create a new context
+const UserContext = createContext();
+
+// Export the created context to be used in other components
+
+export default UserContext;
