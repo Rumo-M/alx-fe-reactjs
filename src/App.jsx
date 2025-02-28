e926b5e33e63f9173fb24059583109243d7bc66a
Martha Morufa <marthamarumo92@mail.com>
2025-02-28 14:55:44 +0200

    updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeList from './components/RecipeList';
-import SearchBar from './components/SearchBar';
+import AddRecipeForm from './components/AddRecipeForm';  
+import RecipeList from './components/RecipeList';        
+import SearchBar from './components/SearchBar';         
 
 const App = () => {
   return (
