85d594a5a8e54337ffdfd934cbe42b65f8129010
Martha Morufa <marthamarumo92@mail.com>
2025-03-01 23:47:49 +0200

    updated the app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import AddRecipeForm from './components/AddRecipeForm';  
-import RecipeList from './components/RecipeList';        
-import SearchBar from './components/SearchBar';         
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
