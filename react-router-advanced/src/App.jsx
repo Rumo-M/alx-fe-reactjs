52e2d885b583ce2ce993869e697ceb90fdaac808
Martha Morufa <marthamarumo92@mail.com>
2025-03-16 17:38:57 +0200

    modified the BlogPost

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from './components/Profile';
+import BlogPost from './components/BlogPost';  // Import BlogPost component
 import NotFound from './pages/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
 import { AuthProvider } from './context/AuthContext';
@@ -17,6 +18,10 @@ function App() {
           {/* Protected route for Profile */}
           <Route path="/profile/*" element={<ProtectedRoute element={<Profile />} />} />
           
+          {/* Dynamic route for blog posts */}
+          <Route path="/blog/:id" element={<BlogPost />} />
+          
+          {/* 404 Not Found route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
