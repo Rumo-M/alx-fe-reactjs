2129eda821e7bb1e0dbbae0729d531fbc6919aee
Martha Morufa <marthamarumo92@mail.com>
2025-03-18 14:02:03 +0200

    updated the TodoList.jsx

--- react-todo/src/component/TodoList.jsx
+++ react-todo/src/component/TodoList.jsx
@@ -28,29 +28,40 @@ function TodoList() {
   const deleteTodo = (id) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
-  
+
   return (
-    <div>
-      <h2>Todo List</h2>
-      <form onSubmit={addTodo}>
+    <div className="max-w-md mx-auto p-4 bg-gray-100 shadow-lg rounded-lg">
+      <h2 className="text-xl font-bold text-center">Todo List</h2>
+      <form onSubmit={addTodo} className="flex gap-2 my-4">
         <input
           type="text"
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
           placeholder="Add a new todo"
-          className="text"
+          className="flex-grow p-2 border rounded"
         />
-        <button className="btn" type="submit">Add</button>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded" type="submit">
+          Add
+        </button>
       </form>
-      <ul>
+      <ul className="space-y-2">
         {todos.map((todo) => (
           <li
             key={todo.id}
             onClick={() => toggleTodo(todo.id)}
+            className="flex justify-between items-center p-2 bg-white shadow rounded cursor-pointer"
             style={{ textDecoration: todo.completed ? "line-through" : "none" }}
           >
             {todo.text}
-            <button className="btn" onClick={(e) => deleteTodo(todo.id, e)}>Delete</button>
+            <button
+              className="bg-red-500 text-white px-3 py-1 rounded"
+              onClick={(e) => {
+                e.stopPropagation();
+                deleteTodo(todo.id);
+              }}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
