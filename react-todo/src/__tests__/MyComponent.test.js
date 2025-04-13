69149a07132b76105dc790d6dee9d41364859795
Martha Morufa <marthamarumo92@mail.com>
2025-04-13 17:23:42 +0200

    Created the MyComponent.test.js file

---
+++ react-todo/src/__tests__/MyComponent.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import MyComponent from './MyComponent';
+
+describe('MyComponent', () => {
+  // tests for MyComponent
+});
